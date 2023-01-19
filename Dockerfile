737e4ff2c543ca9a8b82fbb3ac67f89c8e2e7fa1
Seandon Mooy <seandon.mooy@classdojo.com>
2023-01-19 14:53:41 -0800

    cleanup, update, and add arm64

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
-FROM alpine:3.5
+FROM alpine:3
 
-ENV PERCONA_TOOLKIT_VERSION 3.0.5
+COPY VERSION.txt .
 
 RUN set -x \
+  && export PERCONA_TOOLKIT_VERSION=$(cat VERSION.txt) \
   && apk add --update perl perl-dbi perl-dbd-mysql perl-io-socket-ssl perl-term-readkey make ca-certificates wget \
   && update-ca-certificates \
   && wget -O /tmp/percona-toolkit.tar.gz https://www.percona.com/downloads/percona-toolkit/${PERCONA_TOOLKIT_VERSION}/source/tarball/percona-toolkit-${PERCONA_TOOLKIT_VERSION}.tar.gz \
---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+TAG="erulabs/percona-toolkit:v$(cat VERSION.txt)"
+
+# Enable docker experimental mode:
+#  - echo '{ "experimental": true }' > /etc/docker/daemon.json
+#  - echo '{ "experimental": "enabled" }' > ~/.docker/config.json
+# install buildx (https://github.com/docker/buildx/releases)
+
+# docker buildx create --name mybuilder
+# docker buildx use mybuilder
+# docker buildx inspect --bootstrap
+# docker run --privileged --rm tonistiigi/binfmt --install all
+
+# Troubleshooting:
+# buildx stop... buildx inspect --bootstrap
+
+BUILDX_PREFIX="docker buildx"
+command -v buildx > /dev/null && BUILDX_PREFIX="buildx"
+
+set -e
+# ./bin/install.sh
+
+DOCKER_BUILDKIT=1 ${BUILDX_PREFIX} build --pull --platform linux/amd64,linux/arm64 -t ${TAG} --push .
